a32a240ca3025773ba03546577c8b838b4bc6baa
fescarvalho <fernando_scarvalho@hotmail.com>
2021-12-07 22:16:16 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,55 @@
+//CARREGANDO O EXPRESS
+require('dotenv').config();
+const express = require('express');
+const app = express(); 
+const mongoose = require('mongoose') ;
+mongoose.connect(process.env.CONECTIONSTRING)
+    .then(() => {
+        app.emit('pronto')
+    })
+    .catch(e=> console.log(e))
+  
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash')
+const routes = require('./routes');
+const path = require('path');
+/* const helmet = require('helmet') */
+const csrf = require('csurf')
+const { middlewareGlobal, checkCsrfError, csrfMiddleware }= require('./src/middlewares/middlewares');
+
+/* app.use(helmet()) */
+app.use(express.urlencoded({extended: true})) //Tratando o metodo POST
+app.use(express.json());   
+app.use(express.static(path.resolve(__dirname, 'public')))
+
+const sessionOptions = session({
+    secret: 'SessionOne',
+    store: MongoStore.create({ mongoUrl: process.env.CONECTIONSTRING }),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true
+    }
+})
+
+app.use(sessionOptions);
+app.use(flash());
+
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+app.use(csrf());
+//Nossos proprios middlewares
+app.use(middlewareGlobal);
+app.use(checkCsrfError);
+app.use(csrfMiddleware);
+app.use(routes);
+
+app.on('pronto', () => {
+    app.listen(3000, () => {
+        console.log('Acessar http://localhost:3000')
+        console.log('servidor executando na porta 3000')
+    })
+})
